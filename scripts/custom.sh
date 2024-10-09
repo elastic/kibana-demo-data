735b968c447018d684c7084edee01f07307ef19d
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2024-10-09 15:45:26 +0200

    Make sure correct data views are created for logs

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -116,13 +116,13 @@ process_remote_kibana() {
 
 echo "Installing custom sample data, Kibana part"
 
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-apache_error.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-aws_s3.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-custom_multiplex.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-k8s_container.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-nginx_error.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-nqinx.ndjson"
-process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/log-system_error.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-apache_error.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-aws_s3.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-custom_multiplex.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-k8s_container.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-nginx_error.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-nqinx.ndjson"
+process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/logs-system_error.ndjson"
 process_remote_kibana "https://elastic.github.io/kibana-demo-data/data/custom-metrics-without-timestamp.ndjson"
 
 
